06a57fa1ba55b6ad7fb749427e679701f419239e
amckoy <amckoy@stuy.edu>
2016-01-15 05:31:30 -0500

    Set up Piece class

--- Piece.java
+++ Piece.java
@@ -1,2 +1,5 @@
-public class Piece{//super class for all pieces
+public abstract class Piece{//super class for all pieces
+    public String displayName;
+    public abstract void move();
+    public OrderedPair location = new OrderedPair();
 }
