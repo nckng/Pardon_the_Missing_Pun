0f11bc31168e44a1260ccb7897692b6ccff8a0c2
amckoy <amckoy@stuy.edu>
2016-01-19 05:34:24 -0500

    Add canMove

--- Piece.java
+++ Piece.java
@@ -1,5 +1,6 @@
 public abstract class Piece{//super class for all pieces
     public String displayName;
     public abstract void move();
+    public abstract boolean canMove();
     public OrderedPair location = new OrderedPair();
 }
